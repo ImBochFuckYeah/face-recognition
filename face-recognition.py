9954e863b22a20c3e37919192451b51fdbbd0e31
Nerypg17 <padillanery142@gmail.com>
2024-05-31 23:21:41 -0600

    puerto serial

--- face-recognition.py
+++ face-recognition.py
@@ -1,12 +1,35 @@
 import cv2
 import face_recognition
+import os
 import time
+import tkinter as tk
+from tkinter import simpledialog
+import serial
+
+def load_known_faces(folder_path):
+    known_face_encodings = []
+    known_face_names = []
+
+    for filename in os.listdir(folder_path):
+        if filename.endswith(('.jpg', '.jpeg', '.png')):
+            image_path = os.path.join(folder_path, filename)
+            image = cv2.imread(image_path)
+            face_locations = face_recognition.face_locations(image)
+
+            if face_locations:
+                face_encodings = face_recognition.face_encodings(image, known_face_locations=face_locations)[0]
+                known_face_encodings.append(face_encodings)
+                known_face_names.append(os.path.splitext(filename)[0])
+    
+    return known_face_encodings, known_face_names
 
 def main():
-    # Leer la imagen de comparación y calcular sus codificaciones de rostro
-    image = cv2.imread("images/josue.jpg")
-    face_loc = face_recognition.face_locations(image)[0]
-    face_image_encodings = face_recognition.face_encodings(image, known_face_locations=[face_loc])[0]
+    # Configurar el puerto serial
+    serial_port = serial.Serial('COM9', 9600)  # Ajusta 'COM3' según sea necesario para tu sistema
+
+    # Cargar las codificaciones de rostros conocidos desde una carpeta
+    folder_path = "images"
+    known_face_encodings, known_face_names = load_known_faces(folder_path)
 
     # Inicializar la captura de video
     cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
@@ -30,39 +53,46 @@ def main():
                 for face_location in face_locations:
                     # Obtener las codificaciones del rostro en el frame
                     face_frame_encodings = face_recognition.face_encodings(frame, known_face_locations=[face_location])[0]
-                    
-                    # Comparar las codificaciones del rostro en el frame con la imagen de comparación
-                    result = face_recognition.compare_faces([face_image_encodings], face_frame_encodings)
-                    
-                    # Determinar el texto y color del cuadro en función del resultado de la comparación
-                    if result[0]:
-                        text = "Josue"
+
+                    # Comparar las codificaciones del rostro en el frame con las imágenes de comparación
+                    matches = face_recognition.compare_faces(known_face_encodings, face_frame_encodings)
+                    name = "Desconocido"
+                    color = (50, 50, 255)
+
+                    if True in matches:
+                        match_index = matches.index(True)
+                        name = known_face_names[match_index]
                         color = (125, 220, 0)
+                        serial_port.write(b'T')  # Enviar 'T' al puerto serial
                     else:
-                        text = "Desconocido"
-                        color = (50, 50, 255)
+                        serial_port.write(b'F')  # Enviar 'F' al puerto serial
 
                     # Dibujar el cuadro y el texto en el frame
                     cv2.rectangle(frame, (face_location[3], face_location[2]), (face_location[1], face_location[2] + 30), color, -1)
                     cv2.rectangle(frame, (face_location[3], face_location[0]), (face_location[1], face_location[2]), color, 2)
-                    cv2.putText(frame, text, (face_location[3], face_location[2] + 20), 2, 0.7, (255, 255, 255), 1)
+                    cv2.putText(frame, name, (face_location[3], face_location[2] + 20), 2, 0.7, (255, 255, 255), 1)
 
             # Mostrar el frame con los cuadros y textos
             cv2.imshow("Frame", frame)
-            
+
+            # Capturar tecla presionada
+            key = cv2.waitKey(1) & 0xFF
+
+            # Salir del bucle si se presiona la tecla ESC
+            if key == 27:  # 27 es el código ASCII para ESC
+                break
+
             # Añadir un pequeño retraso para mejorar la capacidad de respuesta
             time.sleep(0.02)
-            
-            # Salir del bucle si se presiona la tecla ESC
-            k = cv2.waitKey(1)
-            if k == 27:  # 27 es el código ASCII para ESC
-                break
+
     except Exception as e:
         print(f"Ocurrió un error: {e}")
     finally:
         # Liberar los recursos de la cámara y cerrar las ventanas
         cap.release()
         cv2.destroyAllWindows()
+        # Cerrar el puerto serial
+        serial_port.close()
 
 if __name__ == "__main__":
     main()
